b3ca1fbe91beeafe5ebd4dd44abcd0e686796071
Wandrey Warley <wandrey09@gmail.com>
2021-06-07 12:11:04 -0300

    pag produtos att para mobile

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ window.onload = () => {
   console.log("Script Start")
 
   
-  trocarLogoQualidade();
+  // trocarLogoQualidade();
 }
 
 const logo = document.querySelector('.logoQualidade');
 const valoresSobre = document.querySelector('.valores');
+// const icone = document.querySelectorAll('.icone');
+// const informacao_produtos = document.querySelector('.informacao_produtos')
 
+icone.addEventListener('click', iconeHandleClick);
 
 
 function trocarLogoQualidade() {
@@ -28,12 +31,24 @@ function pagSobreMobile () {
     valoresSobre.classList.remove('grid-4');
     valoresSobre.classList.add('grid-8');
   } else {
-
+    valoresSobre.classList.add('grid-4');
+    valoresSobre.classList.remove('grid-8');
   }
 
 }
 
-setInterval(() => {
-  trocarLogoQualidade();
-  pagSobreMobile();
-}, 500);
+
+// function iconeHandleClick() {
+//   // icone.classList.add('display_none');
+//   informacao_produtos.classList.toggle('display_none');
+// }
+
+  setInterval(() => {
+    if(document.title === 'Bikecraft - Sobre' || document.title === 'Bikecraft') {
+      trocarLogoQualidade();
+    }
+    if(document.title === 'Bikecraft - Sobre') {
+      pagSobreMobile();
+    }
+  }, 500);
+
